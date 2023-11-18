9a9beedb5b71cdbd656e4f48c3bf53a1513d96ab
김준영 입니다 <137998454+Kjoonyoung@users.noreply.github.com>
2023-11-18 13:21:12 +0900

    Update MailService.java

--- backend/src/main/java/erp/backend/global/mailsender/service/MailService.java
+++ backend/src/main/java/erp/backend/global/mailsender/service/MailService.java
@@ -24,7 +24,7 @@ public class MailService implements MailServiceImpl{
         message.setSubject("(주) 엄용민 프로덕션 : 알림");
 
         String msgg = "";
-        msgg += "비밀번호가 변경되었습니다. 본인이 아닐 시 담당자에게 연락 바람 010-9307-0468 담당자 : 홍길동 부장";
+        msgg += "비밀번호가 변경되었습니다. 본인이 아닐 시 담당자에게 연락 바람 (전화번호 들어갈 자리) 담당자 : 홍길동 부장";
         message.setText(msgg, "utf-8", "html");
 
         message.setFrom(new InternetAddress("umanmyeong@naver.com", "엄용민 프로덕션"));
